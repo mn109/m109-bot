4be7191f52c10a9d45101f281f9447656b0c16a3
Mikhail Nikitin <mkhl.nktn@gmail.com>
2024-01-03 19:13:05 +0400

    feat: update bot features
    Changes include:
    - Added more punchlines to the Divine option.
    - Added proper video buttons.
    - Updated the Donate option.
    - Added the Order option.

--- bot.py
+++ bot.py
@@ -19,8 +19,14 @@ punchlines = [
     ["Перед тем как стать опытным, ты подопытный.", "Исповедь", "https://youtu.be/luRD0QK6a2I"],
     ["Не можешь стать учителем — стань уроком.", "Полубог-полуживотное", "https://youtu.be/B-hMu2j9rLw"],
     ["Победа — полдела, дальше — больше.", "Викрамадитья", "https://youtu.be/lVQEURNbExA"],
+    ["То, что не погубит, помотает нервы, вестимо!", "Костры амбиций", "https://youtu.be/MPwCMyxwVQE"],
+    ["Этот путь вовсе не тупиковый — напротив, только он и ведёт к трону.", "Наверно", "https://youtu.be/V6Ri3PG8Q1Q"],
+    ["Единственный козырь в колоде пока не менялся — это твёрдость руки.", "Так вышло", "https://youtu.be/B2qfITEt7-c"],
+    ["Это типа ковчег, а мы тут за тварей.", "Белая княгиня", "https://youtu.be/I9KUJj2l0I4"],
+    ["Истина порой восходит на нервной почве.", "Кипарис", "https://youtu.be/l5lM6BoRBMk"],
+    ["Вполне хватает твоих действий, чтобы не вникать в смысл твоих слов.", "Позвони (третий раз за вечер)", "https://youtu.be/XGZ842LsPmU"],
     ["Мальчик боится мяча, пока не разобьют лицо.", "Впритык", "https://youtu.be/6w1z0BIt2NM"],
-  #  [],
+    ["Серьёзный мир — только глупости и приятны. Короче, на глупости вся надежда.", "А вдруг", "https://youtu.be/TE9UFNdOgx8"],
     ["Столько людей полыхнуло впустую, как пух — в каком качестве тебя приглашают к столу?", "С любовью", "https://youtu.be/HknPbkMPbHI"],
     ["Релакс — после финала кубка; здесь нет других пенсионных планов.", "Саламандра", "https://youtu.be/MsaSMWoEzUg"],
     ["Идеи не должны попахивать идиотизмом — они должны или не стесняясь разить им, или ходить по стеночке в пределах парадигмы.", "Иметь", "https://youtu.be/fWWpglvBP0M"]
@@ -29,61 +35,79 @@ punchlines = [
 
 def get_random_line() -> str:
     divination = random.choice(punchlines)
-    ready_divination = f"{divination[0]}\n\n\n\n\n«{divination[1]}»\n{divination[2]}"
+    ready_divination = f"{divination[0]}\n\n\n«{divination[1]}»\n{divination[2]}"
     return ready_divination
 
 
 @dp.message(CommandStart())
 async def process_start_command(message: Message):
     await message.answer(
-        'Этот бот отдаёт ссылки на релизы, видео и страницы М в соцсетях, '
-        'гадает на строчках из его треков ',
-        'сообщает о перспективах выхода новой музыки'
-        'и принимает помощь.\n'
-        'Список команд — под кнопкой Menu.'
+        'Привет, это Миша. Я написал этого бота сам. '
+        'Он отдаёт ссылки на релизы, видео и аккаунты в соцсетях, '
+        'гадает на строчках из треков, '
+        'отвечает на слегка набивший мне оскомину вопрос '
+        'о перспективах и сроках выхода новой музыки '
+        'и принимает помощь от благодарных слушателей.\n'
+        'Полный список команд — под кнопкой Menu.'
         )
 
 
 apple_music_button = InlineKeyboardButton(
-    text='Apple Music',
-    url='https://music.apple.com/ru/artist/%D0%BC/1471116485'
+    text="Apple Music",
+    url="https://music.apple.com/ru/artist/%D0%BC/1471116485"
+)
+breaking_and_entering_button = InlineKeyboardButton(
+    text="Клип «Кража со взломом»",
+    url="https://youtu.be/sg7Vig3pFOk"
+)
+radical_poetry_live_button = InlineKeyboardButton(
+    text="«Радикальная поэзия» (Live)",
+    url="https://youtu.be/2eBvgMaZ73Y"
 )
 spotify_button = InlineKeyboardButton(
-    text='Spotify',
-    url='https://open.spotify.com/artist/5HUVgeazK25dgyTE0dMKFN'
+    text="Spotify",
+    url="https://open.spotify.com/artist/5HUVgeazK25dgyTE0dMKFN"
+)
+suppose_button = InlineKeyboardButton(
+    text="Клип «А вдруг»",
+    url="https://youtu.be/1nrV5j6f-Bo"
+)
+suppose_live_button = InlineKeyboardButton(
+    text="«А вдруг» (Live)",
+    url="https://youtu.be/TE9UFNdOgx8"
 )
 vk_music_button = InlineKeyboardButton(
-    text='VK Музыка',
-    url='https://vk.cc/8Ekyio'
+    text="VK Музыка",
+    url="https://vk.cc/8Ekyio"
 )
 vk_public_button = InlineKeyboardButton(
-    text='Паблик в VK',
-    url='https://vk.com/mxxxxxxxxx'
+    text="Паблик в VK",
+    url="https://vk.com/mxxxxxxxxx"
 )
 yandex_music_button = InlineKeyboardButton(
-    text='Яндекс.Музыка',
-    url='https://music.yandex.ru/artist/4703830'
+    text="Яндекс.Музыка",
+    url="https://music.yandex.ru/artist/4703830"
 )
-yandex_tips_button = InlineKeyboardButton(
-    text='Яндекс.Чаевые',
-    url='https://tips.yandex.ru/guest/payment/5730560'
+yandex_tip_button = InlineKeyboardButton(
+    text="Яндекс.Чаевые",
+    url="https://tips.yandex.ru/guest/payment/5730560"
 )
 youtube_channel_button = InlineKeyboardButton(
-    text='YouTube-канал',
-    url='https://www.youtube.com/@mxxxxxxxxx'
-)
-youtube_playlist_button = InlineKeyboardButton(
-    text='Плейлист на YouTube',
-    url='https://www.youtube.com/playlist?list=PLlL8gwnKFC8oPdckcTSvjSZK5OLqkebLM'
+    text="YouTube-канал",
+    url="https://www.youtube.com/@mxxxxxxxxx"
 )
 youtube_music_button = InlineKeyboardButton(
-    text='YouTube Music',
-    url='https://music.youtube.com/channel/UCC1p_NyxmnshXCABr5tlmsg'
+    text="YouTube Music",
+    url="https://music.youtube.com/channel/UCC1p_NyxmnshXCABr5tlmsg"
+)
+youtube_playlist_button = InlineKeyboardButton(
+    text="YouTube",
+    url="https://www.youtube.com/playlist?list=PLlL8gwnKFC8oPdckcTSvjSZK5OLqkebLM"
 )
 
 donations_keyboard = InlineKeyboardMarkup(
     inline_keyboard=[
-        [yandex_tips_button]
+        [yandex_tip_button]
     ]
 )
 
@@ -106,7 +130,10 @@ streaming_keyboard = InlineKeyboardMarkup(
 
 watch_keyboard = InlineKeyboardMarkup(
     inline_keyboard=[
-        [youtube_playlist_button]
+        [suppose_button],
+        [suppose_live_button],
+        [breaking_and_entering_button],
+        [radical_poetry_live_button]
     ]
 )
 
@@ -124,24 +151,26 @@ async def process_learn_command(message: Message):
     await message.answer(
         'У меня порой интересуются, выпущу ли новые треки, '
         'и если да, то когда это счастье наступит. '
-        'Песни выйдут, когда стихнет стрельба — '
-        'я слышал доводы, что каждый должен заниматься своим делом, '
-        'но, к счастью, могу себе позволить не выпускать музыку, '
-        'пока люди натурально гибнут.'
+        'Какие-то песни давно готовы, '
+        'какие-то пишутся прямо сейчас, '
+        'но выйдут они все, когда стихнет стрельба. '
+        'Я слышал доводы, что каждый должен заниматься своим делом, '
+        'но, к счастью, могу себе позволить '
+        'не выпускать музыку, пока гибнут люди.'
         )
 
 
 @dp.message(Command(commands='watch'))
 async def process_watch_command(message: Message):
     await message.answer(
-        text='Плейлист с видео:',
+        text='Видео:',
         reply_markup=watch_keyboard
         )
 
 
 @dp.message(Command(commands='divine'))
 async def process_divine_command(message: Message):
-    time.sleep(1)
+    time.sleep(0.5)
     await message.answer(
         get_random_line()
         )
@@ -155,23 +184,36 @@ async def process_follow_command(message: Message):
         )
 
 
-@dp.message(Command(commands='support'))
-async def process_support_command(message: Message):
+@dp.message(Command(commands='donate'))
+async def process_donate_command(message: Message):
     await message.answer(
-        text='Привет, это Миша. '
-        'Рад, что песни отзываются настолько, '
-        'что возникает желание меня поддержать! '
-        'Под этим сообщением — ссылка на Яндекс.Чаевые.\n'
-        'Но особо ценной — бесценной! — помощью будет, '
-        'если тем или иным образом устроишь мне собеседование '
-        'в какую-нибудь компанию на должность бэкенд-разработчика на Python — '
-        'я сам написал этого бота-визитку, '
-        'использовав библиотеку aiogram, '
-        'чтобы продемонстрировать верхушку айсберга '
-        'моих невероятных навыков программирования :)\n'
-        'Почта: ban.gully.0o@icloud.com',
+        text='Рад, что песни отзываются настолько, '
+        'что возникает желание меня поддержать.\n'
+        'Спасибо.\n'
+        'Миша',
         reply_markup=donations_keyboard
         )
+
+
+@dp.message(Command(commands='order'))
+async def process_order_command(message: Message):
+    await message.answer(
+        text='Привет, это Миша. '
+        'К счастью или несчастью '
+        '(сам всю жизнь думаю то так, то этак), '
+        'музыка никогда не кормила меня.\n'
+        'Я бэкенд-разработчик. '
+        'Написал этого бота на Python, '
+        'использовав библиотеку aiogram — '
+        'во-первых, креативного промо ради, '
+        'а во-вторых — '
+        'чтобы продемонстрировать верхушку айсберга '
+        'моих потрясающих навыков программирования :)\n'
+        'Если тебе нужен свой телеграм-бот — '
+        'или Python backend developer '
+        'с чувством прекрасного — пиши на почту\n'
+        'ban.gully.0o@icloud.com'
+    )
 
 
 @dp.message()
