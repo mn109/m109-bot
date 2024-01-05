4e22829ac8c0d35547df5c8be2c68b502363a5fb
Mikhail Nikitin <mkhl.nktn@gmail.com>
2024-01-05 19:29:35 +0400

    refactor: restructure Telegram bot into modular architecture
    - Split the bot's code into bot.py, handlers.py, data.py, and keyboards.py for improved maintainability and readability.\n- Isolated message handling logic in handlers.py.\n- Separated bot data and message templates into data.py.\n- Organized keyboard layouts in keyboards.py.\n- Ensured functional integrity and consistency post-restructuring.

---
+++ handlers.py
@@ -0,0 +1,93 @@
+from aiogram import Router, F
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
+import keyboards
+import random
+import time
+import data
+
+router = Router()
+
+punchlines = data.punchlines
+
+
+def get_random_line() -> str:
+    divination = random.choice(data.punchlines)
+    ready_divination = f"{divination[0]}\n\n\n«{divination[1]}»\n{divination[2]}"
+    return ready_divination
+
+
+@router.message(CommandStart())
+async def process_start_command(message: Message):
+    await message.answer(data.START_MESSAGE,
+                         reply_markup=keyboards.keyboard
+                         )
+
+
+@router.message(F.text == "Слушать треки")
+@router.message(Command(commands="listen"))
+async def process_listen_command(message: Message):
+    await message.answer(
+        data.LISTEN_MESSAGE,
+        reply_markup=keyboards.streaming_keyboard
+        )
+
+
+@router.message(F.text == "Узнать новости")
+@router.message(Command(commands="learn"))
+async def process_learn_command(message: Message):
+    await message.answer(
+        data.LEARN_MESSAGE,
+        reply_markup=keyboards.keyboard
+        )
+
+
+@router.message(F.text == "Смотреть видео")
+@router.message(Command(commands='watch'))
+async def process_watch_command(message: Message):
+    await message.answer(
+        data.WATCH_MESSAGE,
+        reply_markup=keyboards.watch_keyboard
+        )
+
+
+@router.message(F.text == "Гадать на строчках")
+@router.message(Command(commands='divine'))
+async def process_divine_command(message: Message):
+    time.sleep(0.5)
+    await message.answer(
+        get_random_line()
+        )
+
+
+@router.message(F.text == "Подписаться")
+@router.message(Command(commands="follow"))
+async def process_follow_command(message: Message):
+    await message.answer(
+        data.FOLLOW_MESSAGE,
+        reply_markup=keyboards.social_keyboard
+        )
+
+
+@router.message(F.text == "Поддержать")
+@router.message(Command(commands="donate"))
+async def process_donate_command(message: Message):
+    await message.answer(
+        data.DONATE_MESSAGE,
+        reply_markup=keyboards.donations_keyboard
+        )
+
+
+@router.message(Command(commands="order"))
+async def process_order_command(message: Message):
+    await message.answer(
+        data.ORDER_MESSAGE,
+        reply_markup=keyboards.keyboard
+    )
+
+
+@router.message()
+async def process_other_answers(message: Message):
+    await message.answer(
+        data.OTHER_MESSAGE
+        )
---
+++ keyboards.py
@@ -0,0 +1,107 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup
+
+
+button_1 = KeyboardButton(text="Слушать треки")
+button_2 = KeyboardButton(text="Смотреть видео")
+button_3 = KeyboardButton(text="Узнать новости")
+button_4 = KeyboardButton(text="Гадать на строчках")
+button_5 = KeyboardButton(text="Подписаться")
+button_6 = KeyboardButton(text="Поддержать")
+button_7 = KeyboardButton(text="Заказать бота")
+
+keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [button_1, button_2],
+        [button_3, button_4],
+        [button_5, button_6]
+        ],
+    resize_keyboard=True
+    )
+
+
+apple_music_button = InlineKeyboardButton(
+    text="Apple Music",
+    url="https://music.apple.com/ru/artist/%D0%BC/1471116485"
+)
+breaking_and_entering_button = InlineKeyboardButton(
+    text="Клип «Кража со взломом»",
+    url="https://youtu.be/sg7Vig3pFOk"
+)
+radical_poetry_live_button = InlineKeyboardButton(
+    text="«Радикальная поэзия» (Live)",
+    url="https://youtu.be/2eBvgMaZ73Y"
+)
+spotify_button = InlineKeyboardButton(
+    text="Spotify",
+    url="https://open.spotify.com/artist/5HUVgeazK25dgyTE0dMKFN"
+)
+suppose_button = InlineKeyboardButton(
+    text="Клип «А вдруг»",
+    url="https://youtu.be/1nrV5j6f-Bo"
+)
+suppose_live_button = InlineKeyboardButton(
+    text="«А вдруг» (Live)",
+    url="https://youtu.be/TE9UFNdOgx8"
+)
+telegram_channel_button = InlineKeyboardButton(
+    text="Канал в Telegram",
+    url="https://t.me/mxxxxgram"
+)
+vk_music_button = InlineKeyboardButton(
+    text="VK Музыка",
+    url="https://vk.cc/8Ekyio"
+)
+vk_public_button = InlineKeyboardButton(
+    text="Паблик в VK",
+    url="https://vk.com/mxxxxxxxxx"
+)
+yandex_music_button = InlineKeyboardButton(
+    text="Яндекс.Музыка",
+    url="https://music.yandex.ru/artist/4703830"
+)
+yandex_tips_button = InlineKeyboardButton(
+    text="Яндекс.Чаевые",
+    url="https://tips.yandex.ru/guest/payment/5730560"
+)
+youtube_channel_button = InlineKeyboardButton(
+    text="Канал в YouTube",
+    url="https://www.youtube.com/@mxxxxxxxxx"
+)
+youtube_music_button = InlineKeyboardButton(
+    text="YouTube Music",
+    url="https://music.youtube.com/channel/UCC1p_NyxmnshXCABr5tlmsg"
+)
+
+
+donations_keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [yandex_tips_button]
+    ]
+)
+
+social_keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [telegram_channel_button],
+        [vk_public_button],
+        [youtube_channel_button]
+    ]
+)
+
+streaming_keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [apple_music_button],
+        [spotify_button],
+        [vk_music_button],
+        [youtube_music_button],
+        [yandex_music_button]
+    ]
+)
+
+watch_keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [suppose_button],
+        [suppose_live_button],
+        [breaking_and_entering_button],
+        [radical_poetry_live_button]
+    ]
+)
